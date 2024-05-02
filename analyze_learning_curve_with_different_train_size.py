fb8e830adeed75f4620730d5e323df32627498b8
Belce <belcekaya@gmail.com>
2024-05-02 17:54:57 +0200

    Refactor the code

--- analyze_learning_curve_with_different_train_size.py
+++ analyze_learning_curve_with_different_train_size.py
@@ -1,38 +1,45 @@
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import StandardScaler
+import sklearn
+from sklearn.svm import SVR
+from sklearn.ensemble import GradientBoostingRegressor
+from sklearn.model_selection import learning_curve
+sorted(sklearn.metrics.SCORERS.keys())
+
+# Set different train sizes
 train_sizes = [1, 2, 5, 9,13, 25, 36, 72,90, 108, 126, 135, 144]
 # (180*0.50 - 180*0.80)
 
-
+# Prepare Feature set X and Target Y
 CAO = df.iloc[:,-1].values
-print(CAO) 
 Cikis_AO = pd.DataFrame(data = CAO, index= range(180), columns = ['CAO'])
+
 df  = df.iloc[:,0:33]
-
-
 features = df.columns
 X = df[features] #feature definition
 
-from sklearn.preprocessing import StandardScaler
+# Standardize the Feature Set
 sc= StandardScaler()
-df = sc.fit_transform(df)
 
-df = pd.DataFrame(data=df, index = range(180), columns = [X.columns])
+df_scaled = sc.fit_transform(df)
+df = pd.DataFrame(data=df_scaled, index = range(180), columns = [X.columns])
+
 Cikis_AO = sc.fit_transform(Cikis_AO)
 Cikis_AO = pd.DataFrame(data=Cikis_AO, index = range(180), columns = ['CAO'])
 
 
-import sklearn
-sorted(sklearn.metrics.SCORERS.keys())
+# 1. SVR Training 
 
-##### svr 
-from sklearn.svm import SVR
-from sklearn.model_selection import learning_curve
 features = df.columns
 target = 'CAO'
-train_sizes, train_scores, validation_scores = learning_curve(
-estimator = SVR(),
-X = df[features],
-y = Cikis_AO[target], train_sizes = train_sizes, cv = 10,
-scoring = 'neg_mean_squared_error', shuffle=True)
+
+train_sizes, train_scores, validation_scores = learning_curve(estimator = SVR(),
+                                                              X = df[features],
+                                                              y = Cikis_AO[target], 
+                                                              train_sizes = train_sizes, 
+                                                              cv = 10,
+                                                              scoring = 'neg_mean_squared_error', 
+                                                              shuffle=True)
 
 print('Training scores:\n\n', train_scores)
 print('\n', '-' * 70) # separator to make the output easy to read
@@ -47,7 +54,6 @@ print('\n', '-' * 20) # separator
 print('\nMean validation scores\n\n',pd.Series(validation_scores_mean, 
                                                index = train_sizes))
 
-import matplotlib.pyplot as plt
 
 plt.style.use('seaborn')
 plt.plot(train_sizes, train_scores_mean, label = 'Training error')
@@ -58,17 +64,17 @@ plt.title('Learning curves for a SVR', fontsize = 18 , y = 1.03)
 plt.legend()
 plt.ylim(-3,3)
 
-############# GBM
+# 2. GBM Training
 
-from sklearn.ensemble import GradientBoostingRegressor
-from sklearn.model_selection import learning_curve
 features = df.columns
 target = 'CAO'
-train_sizes, train_scores, validation_scores = learning_curve(
-estimator = GradientBoostingRegressor(),
-X = df[features],
-y = Cikis_AO[target], train_sizes = train_sizes, cv = 10,
-scoring = 'neg_mean_squared_error', shuffle=True)
+train_sizes, train_scores, validation_scores = learning_curve(estimator = GradientBoostingRegressor(),
+                                                              X = df[features],
+                                                              y = Cikis_AO[target], 
+                                                              train_sizes = train_sizes, 
+                                                              cv = 10,
+                                                              scoring = 'neg_mean_squared_error', 
+                                                              shuffle=True)
 
 print('Training scores:\n\n', train_scores)
 print('\n', '-' * 70) # separator to make the output easy to read
@@ -83,8 +89,7 @@ print('\n', '-' * 20) # separator
 print('\nMean validation scores\n\n',pd.Series(validation_scores_mean, 
                                                index = train_sizes))
 
-import matplotlib.pyplot as plt
-
+# Plot the training curve
 plt.style.use('seaborn')
 plt.plot(train_sizes, train_scores_mean, label = 'Training error')
 plt.plot(train_sizes, validation_scores_mean, label = 'Validation error')
